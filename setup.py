4b27d8e2f53c25a514dc111a4426be3df6cc0430
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-10-14 16:25:26 -0400

    Add mutex-watershed evaluation

--- setup.py
+++ setup.py
@@ -10,4 +10,7 @@ setup(
     author_email='jfunke@iri.upc.edu',
     url='http://github.com/funkey/cremi_python',
     packages=['cremi', 'cremi.io', 'cremi.evaluation'],
+    entry_points={'console_scripts': [
+        'eval-watersheds = cremi.eval_watersheds:run_evaluation',
+        'eval-mutex-watersheds = cremi.eval_watersheds_mutex:run_evaluation']}
 )
